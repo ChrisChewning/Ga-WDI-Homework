7932acb483481308591605771f13c7b45e571a13
Chris <chewning.cl@gmail.com>
2018-07-12 22:31:19 -0500

    Commit 2 - added data into vampire collection

--- vampires_app/models/vampire.js
+++ vampires_app/models/vampire.js
@@ -1,5 +1,9 @@
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+//this is so you can refer to a constant that refers to the model. It's a shortcut in a way so you don't have to type require(./vampires) over & over again.
+const Vampire = require('./models/vampire.js')
+
 
 
 
@@ -7,13 +11,22 @@ const vampireSchema = new Schema([{
 //write your schema fields here
   name: {type: String, required: true, unique: true},
   hair_color: {type: String, default: 'blonde'}
-  eye_color: {type: String},
-  dob: {type: Date},
+  eye_color: String,
+  dob: Date,
   loves: {type: Array},
-  location: {type: String},
-  gender: {type: String, maxlength: 1}, //how to restrict a string to m or f only?
+  location: String,
+  gender: {type: String, enum: ['m', 'f', 't']},
+  //^maxlength: 1 was good but didn't restrict to m or f
   victims: {type: Number, min: 0},
 }]);
 
 
-loves: ['cereal','marshmallows'],
+//To put into mongo: Mongoose injects your model into mongodb
+//the first argument will be the name of your mongo collection.
+//the second argument will be what those documents look like.
+const Vampires = mongoose.model('Vampires', vampireSchema); //need to be vampires instead?
+
+
+
+
+module.exports = Vampires; //you are exporting the const you made, which has two arguments in it.
--- vampires_app/populateVampires.js
+++ vampires_app/populateVampires.js
@@ -1,38 +1,78 @@
-const vampireSchema = new Schema(
+//NOTES:
+//the express app is on its own server.
+//the database is on its own server.
 
-  [{
-//write your schema fields here
-  name: {type: String, required: true, unique: true},
-  hair_color: {type: String}
-  eye_color: {type: String, required: true, unique: true},
-  dob: {type: String, required: true, unique: true},
-  loves: {type: String, required: true, unique: true},
-  location: {type: String, required: true, unique: true},
-  gender: {type: String, },
-  victims: {type: String, required: true, unique: true},
-});
+const mongoose = require('mongoose');
+const database = mongoose.connection;
+
+//this model will be able to update, get, delete, etc. from our database.
+const Vampires = require('./models/vampires');
+mongoose.connect('mongodb://localhost:27017/vampire');
 
 
-
-// const vampire = {
-//   name: 'Count Chocula',
-//   hair_color: 'brown',
-//   eye_color: 'brown',
-//   dob: new Date(1971, 2, 13, 7, 47),
-//   loves: ['cereal','marshmallows'],
-//   location: 'Minneapolis, Minnesota, US',
-//   gender: 'm',
-//   victims: 2,
-// }
+// Using the create method, create 4 new vampires with any qualities that you like two should be male and two should be female.
+// 🔴 The commit message should read:
+// "Commit 2 - added data into vampire collection"
 
 
-//   {
-//     name: 'Count Chocula',
-//     hair_color: 'brown',
-//     eye_color: 'brown',
-//     dob: new Date(1971, 2, 13, 7, 47),
-//     loves: ['cereal','marshmallows'],
-//     location: 'Minneapolis, Minnesota, US',
-//     gender: 'm',
-//     victims: 2
-//   },{
+//-------------------------  CHECK FOR CONNECTION  -----------------------------
+
+//ERROR CHECK db.on is the same format as app.listen
+db.on('error', (err) => {
+  //error message of a a failed connection
+  //error is a reserved keyword. If there is an error when we attempt to connect, this will log out.
+  console.log(err, ' this is the error message')
+})
+
+//CONNECTED CHECK
+db.on('connected', () => {
+  //connection is a reserved keyword
+  console.log('mongoose is connected to mongodb');
+})
+
+//-------------------------  CHECK FOR CONNECTION  -----------------------------
+
+
+    Vampire.create({
+    name: 'Van',
+    hair_color: 'black',
+    eye_color: 'brown',
+    dob: new Date(1978, 2, 13, 7, 47),
+    loves: ['rare meat','Frito Pie'],
+    location: 'Minneapolis, Minnesota, US',
+    gender: 'm',
+    victims: 25
+    })
+
+    Vampire.create({
+    name: 'Drag-you-luh',
+    dob: new Date(937, 0, 24, 13, 0),
+    hair_color: 'black',
+    eye_color: 'blue',
+    loves: ['La Croixs', 'steam engines', 'potatoes', 'side pieces'],
+    location: 'Transylvania, Romania',
+    gender: 'f',
+    victims: 1238
+      })
+
+      Vampire.create({
+      name: 'Vamboni',
+      hair_color: 'brown',
+      eye_color: 'green',
+      dob: new Date(2004, 10, 12, 5, 12),
+      loves: ['baseball at night'],
+      location: 'Baltimore, Maryland, US',
+      gender: 'm',
+      victims: 10
+    })
+
+      Vampire.create({
+      name: 'Sneakz',
+      dob: new Date(92, 22, 42, 8, 1),
+      hair_color: 'black',
+      eye_color: 'blue',
+      loves: ['Soap', 'Socks', 'Sandwiches'],
+      location: 'Northwest, Canada',
+      gender: 'f',
+      victims: 1
+    })
