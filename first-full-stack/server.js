7dba51be04ef89f0443e5811b2a512f92aa7d597
Chris <chewning.cl@gmail.com>
2018-07-15 15:22:04 -0500

    server.js finished until controllers

--- first-full-stack/server.js
+++ first-full-stack/server.js
@@ -5,6 +5,19 @@ const port = 3000;
 require('./db/db');
 
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+  extended: false;
+}))
+
+const methodOverride = require('method-override');
+app.use(methodOverride('method'));
+
+
+
+
+
+
 app.listen(port, () => {
   console.log('listening on port 3000');
 })
