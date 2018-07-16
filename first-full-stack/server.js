fd7959f09065e7dbc5c3d598c8941706ebe28d15
Chris <chewning.cl@gmail.com>
2018-07-15 22:21:41 -0500

    got the show route working

--- first-full-stack/server.js
+++ first-full-stack/server.js
@@ -7,14 +7,14 @@ const Doggo = require('./models/goodDog')
 require('./db/db');
 
 
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({
   extended: false
-}))
+}));
 
 const methodOverride = require('method-override');
-app.use(methodOverride('method'));
-
+app.use(methodOverride('_method'));
 
 
 
@@ -32,7 +32,7 @@ app.get('/home', (req, res) => {
 
 //---------------------- NEW ROUTE -------------------------
 
-app.get('/new', (req, res) => {
+app.get('/home/new', (req, res) => {
   // res.send('this is for new'); for testing
   res.render('new.ejs')
   });
@@ -41,7 +41,7 @@ app.get('/new', (req, res) => {
 
 //---------------------- POST Route creates a request -------------------------
 
-app.post('/', (req, res) => {
+app.post('/home', (req, res) => {
   Doggo.create(req.body, (err, createDog) => {
   res.redirect('/home')
   })
@@ -59,7 +59,32 @@ app.get('/home/:id/edit', (req, res) => {
 });
 
 
-//---------------------- NEW ROUTE -------------------------
+
+//---------------------- UPDATE ROUTE -------------------------
+
+app.put('/:id', (req, res) => {
+  Doggo.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, updateDog => {
+    if(err){
+            res.send(err);
+          } else {
+        //check to see if it is updating correctly.
+        console.log(updateDog, 'check our model');
+    res.redirect('/home');
+  }
+  })
+)
+})
+
+
+
+//---------------------- DELETE ROUTE -------------------------
+
+app.delete('/:id', (req, res) => {
+  Doggo.findByIdAndDelete(req.params.id, (err, deleteDog) => {
+    res.redirect('/home')
+    doggo: deleteDog
+  })
+})
 
 
 
@@ -67,7 +92,35 @@ app.get('/home/:id/edit', (req, res) => {
 //---------------------- SHOW ROUTE -------------------------
 
 
+// Show Route
+app.get('/:id', (req, res) => {
 
+  // Render is when you want to send
+  // an ejs template to the client
+  Doggo.findById(req.params.id, (err, databaseArray) => {
+
+    if(err){
+      console.log(err, ' this is error in show');
+      res.send(err);
+    } else {
+//check to see if it is updating correctly.
+  // console.log(deletedFruit, ' this is the deletedFruit in the delete route');
+
+      res.render('show.ejs', {
+
+        //think this part may be wrong.
+      doggos: databaseArray// This creates a "tvshow" variable in the show page
+    })
+  }
+  })
+});
+
+
+
+
+
+
+//---------------------- PORT -------------------------
 
 app.listen(port, () => {
   console.log('listening on port 3000');
